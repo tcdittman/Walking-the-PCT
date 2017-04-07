50155c9df7376b3d25d5b8f7c274dec35c6459b3
Tyler Dittman <tcdittman@gmail.com>
2017-04-06 19:01:03 -0700

    made the progress map marker work dynamically

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /*
 Technical challenges:
-- oauth token
+- oauth token - emailed Dev who wrote it
 - each day having to refresh token
 */
 
@@ -68,18 +68,42 @@ $(document).ready(function() {
 
   var urlStart = 'https://wbsapi.withings.net/v2/measure?action=getactivity&startdateymd=2017-01-01';
 
-  var tokenAppend = '&access_token=f855c3a7396218f1888dd2d556ece7520d2e210e';
+  var tokenAppend = '&access_token=8a62236f74b0b1c5d2e504fe7f2622bd90726276';
 
   var totalSteps = 0;
   var sumSteps = function(results) {
 
     results.body.activities.forEach(function(day) {
-      totalSteps += (day.steps);
-      console.log(day.steps);
+      if (day.steps === 0) {}
+      else {
+        totalSteps += (day.steps);
+        console.log("this day's steps: " + day.steps);
+        console.log("miles travelled this day: " + Math.round(day.steps/1973.8318));
+      }
     });
-    console.log("total steps = " + totalSteps);
+    console.log("total annual steps = " + totalSteps);
+
+    var annualMiles = Math.round(totalSteps/1973.8318);
+    console.log("total annual miles = " + annualMiles);
+
+    var percentOfPct = (annualMiles/2650*100);
+    console.log("this percentage of the PCT: " + percentOfPct);
+
+    var roundedPercentOfPct = (Math.round(percentOfPct * 100) / 100);
+    console.log("rounded percentage of the PCT: " + roundedPercentOfPct);
+
+    var roundedPercentOfMap = (Math.round(percentOfPct * 100 * .9) / 100) + "%";
+    console.log("rounded percentage for the map: " + roundedPercentOfMap);
+
+    $('.pctprogressmapmarker').css('width', roundedPercentOfMap);
+
+
+    console.log("or this percentage of the JMT: " + (Math.round(annualMiles/210*100)));
+
     var $yearSteps = $('<p>I\'ve walked ' + totalSteps + ' steps this year.</p>');
-    $("#today").append($yearSteps);
+    var $yearMiles = $('<p>That\'s about ' + annualMiles + ' miles "hiked" this year.</p>');
+    var $yearMiles = $('<p>Which is ' + roundedPercentOfPct + '% of the PCT.</p>');
+    $("#today").append($yearSteps, $yearMiles);
 
   };
   $.get(urlStart + endDate + tokenAppend, sumSteps);
